c30048f292e44d52bf3c3b3fe0acb6deae80e6d3
StevenDRiggs <sfriggs21@gmail.com>
2021-06-15 21:30:03 -0600

    basic file structure established; began adding elements (logos)

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
-module.exports = {
+//module.exports = {
+//  reactStrictMode: true,
+//  images: {
+//    domains: ['www.cbinsights.com'],
+//  },
+//}
+const withPlugins = require('next-compose-plugins')
+const withImages = require('next-images')
+
+const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['www.cbinsights.com']
+  },
 }
+
+module.exports = withPlugins([[withImages]], nextConfig)
---
+++ pages/newsletter.tsx
@@ -0,0 +1,13 @@
+import styles from '../styles/Newsletter.module.css'
+
+
+function Newsletter() {
+  return (
+    <div>
+      Newsletter
+    </div>
+  )
+}
+
+
+export default Newsletter
